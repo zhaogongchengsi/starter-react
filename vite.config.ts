b76468353d8345869f7de25d8d70a17c6e5c2b90
zhaogongchengsi <zzh1586169624@163.com>
2023-02-18 16:20:02 +0800

    feat: mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,45 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import UnoCSS from "unocss/vite";
 import presetAttributify from "unocss/preset-attributify";
 import presetIcons from "unocss/preset-icons";
 import presetUno from "unocss/preset-uno";
 import { fileURLToPath } from "url";
+import { viteMockServe } from "vite-plugin-mock";
 
-export default defineConfig(() => {
-  const dark = "class";
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const dark = "class",
+    proxyprefix = env.VITE_PROXY,
+    proxytraget = env.VITE_TARGET,
+    usemock = env.VITE_USE_MOCK === "true" ? true : false;
 
   return {
+    server: {
+      port: 3531,
+      proxy: {
+        [`^${proxyprefix}/.*`]: {
+          target: proxytraget + proxyprefix,
+          changeOrigin: true,
+          rewrite: (path: string) => {
+            const reg = new RegExp("^\\" + proxyprefix + "/");
+            return path.replace(reg, "");
+          },
+        },
+      },
+    },
+
     plugins: [
       UnoCSS({
         presets: [presetUno({ dark }), presetAttributify({ dark }), presetIcons({})],
       }),
       react(),
+      usemock &&
+        viteMockServe({
+          mockPath: "mock", //mock文件路径，在根路径下创建一个mock文件
+          prodEnabled: false, //生产环境下为false，这样就不会被打包到生产包中
+          ignore: /^\_/, //忽略开始_路径
+        }),
     ],
 
     resolve: {
