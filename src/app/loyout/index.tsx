f1e494976f8a4d84ab3a38976e74ce5880501ede
zhaogongchengsi <zzh1586169624@163.com>
2023-04-12 14:36:36 +0800

    初步实现动态布局

--- src/app/loyout/index.tsx
+++ src/app/loyout/index.tsx
@@ -3,22 +3,18 @@ import { LayoutProps } from "./props";
 import { Layout } from "@arco-design/web-react";
 import React from "react";
 
-const AppLayout :React.FC<LayoutProps> = ({ children, aside, header }) => {
-
-    return (
-        <Layout className="w-full h-screen flex-row!">
-          <Slider> {aside} </Slider>
-          <Layout>
-            <Layout.Header>
-                {header}
-            </Layout.Header>
-            <Layout>
-                <Layout.Content>
-                {children}
-                </Layout.Content>
-            </Layout>
-          </Layout>
+const AppLayout: React.FC<LayoutProps> = ({ children, aside, header }) => {
+  return (
+    <Layout className="w-full h-screen flex-row!">
+      <Slider> {aside} </Slider>
+      <Layout>
+        <Layout.Header>{header}</Layout.Header>
+        <Layout>
+          <Layout.Content>{children}</Layout.Content>
         </Layout>
+      </Layout>
+    </Layout>
   );
-}
-export default AppLayout
+};
+
+export default AppLayout;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,3 @@
-import { createStore } from "jotai";
 import { useTheme, store as themeStore } from  '@/store/theme'
 
 export {
